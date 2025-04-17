55f14eaf9143dfdf5f78ed172cf264f9fc5a4184
Loki17 <Tkl33pac@proton.me>
2025-04-17 15:53:22 +0200

    first set up Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Home = () => {
+  const links = [
+    { label: "Calcul", path: "/calculation" },
+    { label: "Orientation", path: "/orientation" },
+    { label: "Détection d'erreurs", path: "/error-detection" },
+    { label: "Catégories de mots", path: "/words-categories" },
+  ];
+
   return (
-    <div className="bg-gray-100  p-4">
-      <h1>Bienvenue sur mon app</h1>
-      <p>C'est un exemple avec un fond clair et un texte sombre.</p>
+    <div
+      id="Hello"
+      className="min-h-screen w-full flex justify-evenly items-center flex-col gap-5"
+    >
+      {links.map((link) => (
+        <div key={link.label}>
+          <Link
+            className="card card-hover flex items-center justify-center w-xs h-28 text-xl font-bold active:scale-95 hover:scale-[1.02]"
+            to={link.path}
+          >
+            {link.label}
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
