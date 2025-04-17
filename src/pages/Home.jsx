5d166d39be6a79e8cb87dbb1edd03d5dc58fa1fb
Loki17 <Tkl33pac@proton.me>
2025-04-17 15:31:36 +0200

    first styling for the body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-gray-100  p-4">
+      <h1>Bienvenue sur mon app</h1>
+      <p>C'est un exemple avec un fond clair et un texte sombre.</p>
+    </div>
+  );
 };
 
 export default Home;
