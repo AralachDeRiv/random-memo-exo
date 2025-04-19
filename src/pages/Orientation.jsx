efd65e137a3b9e311cfa3a5c04b1ddf80f2445bc
Loki17 <Tkl33pac@proton.me>
2025-04-19 19:41:02 +0200

    start orientation + svg arrows

--- src/pages/Orientation.jsx
+++ src/pages/Orientation.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import BackHomeBtn from "../components/BackHomeBtn";
 
 const Orientation = () => {
-  return <div>Orientation</div>;
+  return (
+    <div className="min-h-screen w-full pt-24 px-5 flex items-center justify-center">
+      <BackHomeBtn />
+    </div>
+  );
 };
 
 export default Orientation;
