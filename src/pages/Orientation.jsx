5a1e4d03b10471fcedc545b16bcb795204d2a0f5
Loki17 <Tkl33pac@proton.me>
2025-04-19 20:12:34 +0200

    orientation settings form

--- src/pages/Orientation.jsx
+++ src/pages/Orientation.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import BackHomeBtn from "../components/BackHomeBtn";
+import OrientationsSettingsForm from "../components/orientation/OrientationsSettingsForm";
+
+// const directionToDegrees = {
+//   "up-right": 45,
+//   "up-left": 315,
+//   "down-right": 135,
+//   "down-left": 225,
+// };
 
 const Orientation = () => {
+  const [numberOfQuestions, setNumberOfQuestions] = useState(null);
+
   return (
     <div className="min-h-screen w-full pt-24 px-5 flex items-center justify-center">
       <BackHomeBtn />
+
+      {!numberOfQuestions && (
+        <OrientationsSettingsForm setNumberOfQuestions={setNumberOfQuestions} />
+      )}
     </div>
   );
 };
