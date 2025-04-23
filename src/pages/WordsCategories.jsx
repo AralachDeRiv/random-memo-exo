7b54bfe18f3c92c209cc6be75a3cc3eabd791070
Loki17 <Tkl33pac@proton.me>
2025-04-23 15:09:02 +0200

    settingsform

--- src/pages/WordsCategories.jsx
+++ src/pages/WordsCategories.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import BackHomeBtn from "../components/BackHomeBtn";
+import WordsCategoriesSettingsForm from "../components/words-categories/WordsCategoriesSettingsForm";
 
 const WordsCategories = () => {
-  return <div>WordsCategories</div>;
+  const [numberOfQuestions, setNumberOfQuestions] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [finished, setFinished] = useState(false);
+  const endTimeRef = useRef(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  return (
+    <div className="min-h-screen w-full pt-24 px-5 flex items-center justify-center">
+      <BackHomeBtn />
+
+      {!numberOfQuestions && !finished && (
+        <WordsCategoriesSettingsForm
+          setNumberOfQuestions={setNumberOfQuestions}
+          setIsRunning={setIsRunning}
+        />
+      )}
+    </div>
+  );
 };
 
 export default WordsCategories;
